a6f8d93a99bfc2cf44a221f04a3628ebcca85169
Digital <bogdan.kovalev.job@gmail.com>
2025-10-09 11:03:19 +0100

    Update MarketInfoProvider.ts

--- src/gas/providers/MarketInfoProvider.ts
+++ src/gas/providers/MarketInfoProvider.ts
@@ -1,6 +1,7 @@
 import {
   type ICandidatesDao,
   type IMarketDataDao,
+  StableUSDCoin,
   ExchangeSymbol,
   SymbolStatus,
 } from "../../lib/Types";
@@ -37,7 +38,7 @@ export class MarketInfoProvider {
     this.mktDataDao.updateDemandHistory(() => imbalance, step);
 
     const mktPercentile = this.mktDataDao.getStrength(imbalance.average);
-    const btcCur = this.plugin.getPrices()[`BTC${stableCoin}`];
+    const btcCur = this.plugin.getPrices()[`BTC${StableUSDCoin.USDT}`];
     const btcDaily = this.plugin.getDailyPrices().BTC;
 
     if (!btcCur || !btcDaily?.prices[0]) {
