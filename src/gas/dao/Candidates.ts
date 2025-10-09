fdc2f67a89a5ec048e62e931b436868261ffd599
Digital <bogdan.kovalev.job@gmail.com>
2025-10-09 10:56:35 +0100

    Update Candidates.ts

--- src/gas/dao/Candidates.ts
+++ src/gas/dao/Candidates.ts
@@ -62,15 +62,12 @@ export class CandidatesDao implements ICandidatesDao {
       return;
     }
 
-    this.store.update<Record<string, CandidateInfo>>(
-      CandidatesDataKey,
-      (all) => {
-        if (all?.[coin]) {
-          all[coin][Key.PINNED] = value ? Bit.TRUE : Bit.FALSE;
-        }
-        return all;
-      },
-    );
+    this.update((all) => {
+      if (all[coin]) {
+        all[coin][Key.PINNED] = value ? Bit.TRUE : Bit.FALSE;
+      }
+      return all;
+    });
 
     Log.info(`${coin} ${value ? `` : `un`}pinned`);
   }
